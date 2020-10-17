5769e2edbc5c642b5b217f8dfe96c5271d439af7
Dawid Ginter <dginter89@gmail.com>
2020-10-17 22:53:53 +0200

    Zmiany z funckją zapisu do csv

--- lab1.py
+++ lab1.py
@@ -1,11 +1,24 @@
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 import os
+import csv
+from numpy import asarray
+from numpy import savetxt
 
-data = load_wine()
 
-# Wyświwtlenie całej struktury data 
-print(data.data)
+# Generujemy zestaw danych
+X, y = load_wine(return_X_y=True)
+
+# Wyświwtlenie całej struktury zwracanej przez metodę load_wine zad. 1
+print(load_wine)
+
+# Przetwarzanie danych za pomocą train_test_split - zadanie 2
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+print(y_train)
+print(X_train)
+print("Długość tablicy X_test: ", len(X_test))
 
 # Sprawdź czy istnieje folder out, jeśli nie to utwórz go 
 dir = os.path.join(os.getcwd(), "out")
@@ -14,3 +27,8 @@ print(os.getcwd())
 
 if not os.path.exists(dir):
     os.mkdir(dir)
+
+
+def print_to_csv(data_array, file_name):
+    savetxt(fname=file_name, X=data_array, delimiter=',')
+
